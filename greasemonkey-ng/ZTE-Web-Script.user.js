aa297917a67e687c5fcfc4db457c5050dab92584
Thomas Pöchtrager <t.poechtrager@gmail.com>
2025-12-13 20:05:41 +0100

    Add greasemonkey user script for NG version

---
+++ greasemonkey-ng/ZTE-Web-Script.user.js
@@ -0,0 +1,62 @@
+// ==UserScript==
+// @name         ZTE-Web-Script
+// @version      0.2
+// @description  Automatically loading ZTE-Web-Script
+// @match        https://192.168.0.1/*
+// @match        https://192.168.1.1/*
+// @match        https://192.168.2.1/*
+// @match        http://192.168.0.1/*
+// @match        http://192.168.1.1/*
+// @match        http://192.168.2.1/*
+// @require      https://code.jquery.com/jquery-3.7.1.min.js
+// @grant        GM_xmlhttpRequest
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function handleResponse(responseText, onload) {
+        var script = document.createElement('script');
+        script.textContent = responseText;
+        document.head.appendChild(script);
+        if (onload) onload();
+    }
+
+    function loadScript(url, onload, onerror) {
+        // Add timestamp to the URL to bypass cache
+        var timestamp = new Date().getTime();
+        var urlWithTimestamp = url + "?t=" + timestamp;
+
+        if (typeof GM_xmlhttpRequest !== "undefined") {
+            GM_xmlhttpRequest({
+                method: "GET",
+                url: urlWithTimestamp,
+                onload: function(response) {
+                    handleResponse(response.responseText, onload);
+                },
+                onerror: onerror
+            });
+        } else {
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", urlWithTimestamp, true);
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState === 4) {
+                    if (xhr.status === 200) {
+                        handleResponse(xhr.responseText, onload);
+                    } else {
+                        if (onerror) onerror();
+                    }
+                }
+            };
+            xhr.onerror = onerror;
+            xhr.send();
+        }
+    }
+
+    loadScript("https://cdn.jsdelivr.net/gh/tpoechtrager/ZTE-Web-Script/legacy/zte-script-legacy.js", function() {
+        console.log("ZTE-Web-Script loaded and executed.");
+    }, function() {
+        alert("Failed to load the ZTE-Web-Script");
+    });
+
+})();
